d4a5a5f4a6a5b0ba2da2f882037aeef5c85a4982
Daniel8529 <daniel_montero@ucne.edu.do>
2022-02-01 23:54:35 -0800

    Creacion de registro y consulta

---
+++ UI/Registros/cRegistros.cs
@@ -0,0 +1,52 @@
+
+using System.Windows;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Daniel_Montero_Ap1_p1.UI.Registros
+{
+    public partial class cRegistros : Window
+    {
+       
+
+        public cRegistros()
+        {
+            InitializeComponent();
+
+         
+        }
+
+        private void Cargar()
+        {
+
+        }
+
+        private void Limpiar()
+        {
+            
+        }
+
+       
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+       
+        }
+
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+    }
+}
