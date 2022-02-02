30ba2303558a4bdb224def15fcd5e3115e91423a
Daniel8529 <daniel_montero@ucne.edu.do>
2022-02-01 22:45:51 -0800

    Creacion de UI y consulta y Registro

---
+++ UI/Consultas/cLibros.xaml.cs
@@ -0,0 +1,21 @@
+using System.Windows;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Daniel_Montero_Ap1_p1.UI.Consultas
+{
+    public partial class cLibros : Window
+    {
+
+        public cLibros()
+        {
+            //InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
