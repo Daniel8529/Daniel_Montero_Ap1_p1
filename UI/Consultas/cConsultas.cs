19926421fa5d263b9737ebf40d33fa62dcb8b20b
Daniel8529 <daniel_montero@ucne.edu.do>
2022-02-01 23:23:22 -0800

    d

--- UI/Consultas/cConsultas.cs
+++ UI/Consultas/cConsultas.cs
@@ -1,19 +1,19 @@
 using System.Windows;
 using System.Linq;
 using System.Collections.Generic;
-
 namespace Daniel_Montero_Ap1_p1.UI.Consultas
 {
-    public partial class cConsultas: Window
+    public partial class cConsultas : Window
     {
 
         public cConsultas()
         {
-            //InitializeComponent();
+            InitializeComponent();
         }
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+          
 
         }
 
