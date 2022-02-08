353fdfec084b32ecff4185de3ffc125ccbe289b8
Daniel8529 <daniel_montero@ucne.edu.do>
2022-02-07 18:38:37 -0800

    Daniel

---
+++ Migrations/20220208023825_inicial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Daniel_Montero_Ap1_p1.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false),
+                    Costo = table.Column<int>(type: "INTEGER", nullable: false),
+                    ValorInventario = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_productos", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "productos");
+        }
+    }
+}
