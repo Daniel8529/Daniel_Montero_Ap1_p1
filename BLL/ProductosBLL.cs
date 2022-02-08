acc2aeaf6ac3b4fc07f525c18ac4cbd54f585a11
Daniel8529 <daniel_montero@ucne.edu.do>
2022-02-07 19:33:00 -0800

    Daniel

--- BLL/ProductosBLL.cs
+++ BLL/ProductosBLL.cs
@@ -8,21 +8,107 @@ using Daniel_Montero_Ap1_p1.DAL;
 
 namespace Daniel_Montero_Ap1_p1.BLL
 {
-   public class ProductosBLL{
+    public class ProductosBLL
+    {
 
-      public static bool insertar (Productos inseta)
-      {
-          bool paso=false;
-          using(var contexto=new  Contexto())
-          {
-              contexto.Productos.Add(inseta);
-              paso=contexto.SaveChanges()>0;
-          }
+        public static bool insertar(Productos inseta)
+        {
+            bool paso = false;
+            using (var contexto = new Contexto())
+            {
+                contexto.Productos.Add(inseta);
+                paso = contexto.SaveChanges() > 0;
+            }
+            return paso;
+        }
+
+        public static bool Editar(Productos editar)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Entry(editar).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+
+
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+            return paso;
+        }
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                var adicionales = contexto.Productos.Find(id);
+                if (adicionales != null)
+                {
+
+
+                    contexto.Productos.Remove(adicionales);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+            return paso;
+
         
       }
+      public static List<Productos> GeLista()
+      {
+           using (var contexto = new Contexto())
+            {
+                     return contexto.Productos.ToList();
+            }
+      
+
+      }
+        /* public static List<Productos> GetList(Expression <Func<Productos,bool>> criterio)
+           {
+               Contexto contexto =new Contexto();
+               List<Productos> lista =new List<Productos>();
+               try
+               {
+                   lista=contexto.Productos.Where(criterio).ToList();
+
+               }
+               catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+            return lista;
+
+           }
+           */
 
 
 
-   }
-    
+    }
+
 }
