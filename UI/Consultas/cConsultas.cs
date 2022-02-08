eaaa57457adafdbd21294ef71bcecfc1a71b594d
Daniel8529 <daniel_montero@ucne.edu.do>
2022-02-07 21:26:53 -0800

    Daniel

--- UI/Consultas/cConsultas.cs
+++ UI/Consultas/cConsultas.cs
@@ -17,6 +17,13 @@ namespace Daniel_Montero_Ap1_p1.UI.Consultas
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
             var lista = new List<Productos>();
+            if(FiltroComboBox.Text=="consulta")
+            {
+                if(string.IsNullOrWhiteSpace(CriterioTextBox.Text))
+                {
+                    // lista=ProductosBLL.GeLista(l=> true);
+                }
+            }
           
 
         }
